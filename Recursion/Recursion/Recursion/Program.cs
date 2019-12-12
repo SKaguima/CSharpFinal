86d623c1a91b7ad483d3fb840ac859c909c554ea
Stan Kaguima <54599064+SKaguima@users.noreply.github.com>
2019-12-12 02:18:15 -0800

    Recursion Study and Practice
    This project was from the instructor as we learned recursion.

---
+++ Recursion/Recursion/Recursion/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Launch(-10);
+
+            Hanoi(1, 'A', 'C', 'B');
+
+            Console.WriteLine(   IsEven(78)  );
+            
+        }
+
+        public static bool IsEven(int num)
+        {
+            if (num % 2 == 0)
+                return true;
+            else
+                return false;
+        }
+
+        public static void Hanoi(int n, char from, char to, char intermediary)
+        {
+            if (n > 0)
+            { 
+                Hanoi(n - 1, from, intermediary, to);
+                Console.WriteLine(from + " -> " + to);
+                Hanoi(n - 1, intermediary, to, from);
+            }
+        }
+
+
+        static void Launch(int num)
+        {
+            if (num > 0)
+            {
+                Console.WriteLine(num);
+                Launch(num - 1);
+            }
+            else //base case
+            {
+                Console.WriteLine("Blastoff");
+            }
+        }
+    }
+}
