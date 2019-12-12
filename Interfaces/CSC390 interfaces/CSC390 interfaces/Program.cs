484f82d85bab3636f9359cdca11583e5621421e7
Stan Kaguima <54599064+SKaguima@users.noreply.github.com>
2019-12-12 02:15:30 -0800

    Interfaces Practice
    This program was a practice of how to use interfaces with classes.

---
+++ Interfaces/CSC390 interfaces/CSC390 interfaces/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC390_interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int[] arr = { 1, 2, 3 };
+            //Console.WriteLine(arr[5]);
+            try
+            {
+                //ThrowExcp();
+                int[] arr = { 1, 2, 3 };
+                Console.WriteLine(arr[5]);
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp.Message);
+            }
+
+
+            Student myStudend = new Student();
+            Cat myCat = new Cat();
+
+            TestME(myCat);
+            TestME(myStudend);
+        }
+
+
+        static void ThrowExcp()
+        {
+            throw new IndexOutOfRangeException("IndexOutOfRangeException was thrown inside MSSA course");
+        }
+
+
+
+        static void TestME(IGivesHello instance)
+        {
+            instance.SayHello();
+        }
+
+        interface IGivesHello
+        {
+            void SayHello();
+        }
+
+        class Student: IGivesHello
+        {
+            public string Name { get; set; }
+
+            public void SayHello()
+            {
+                Console.WriteLine("Hello, my name is: {0}", Name);
+            }
+        }
+
+        class Cat: IGivesHello
+        {
+            public int NumLegs { get; set; }
+
+            public void SayHello()
+            {
+                Console.WriteLine("Meow with {0} legs", NumLegs);
+            }
+        }
+    }
+}
