0896cd6d66397c291754ec9f786f5c933a27973c
Stan Kaguima <54599064+SKaguima@users.noreply.github.com>
2019-12-12 02:43:04 -0800

    Data Structures Study and Practice
    Studying and practicing different data structures and their applications

---
+++ Exploring data structures/CSC390 data structures/CSC390 data structures/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC390_data_structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> mylist = new List<int>();
+            Console.WriteLine("count = {0}", mylist.Count);
+            Console.WriteLine("Capacity = {0}", mylist.Capacity);
+
+            mylist.Add(5);
+            Console.WriteLine("count = {0}", mylist.Count);
+            Console.WriteLine("Capacity = {0}", mylist.Capacity);
+
+            mylist.Add(2);
+            Console.WriteLine("count = {0}", mylist.Count);
+            Console.WriteLine("Capacity = {0}", mylist.Capacity);
+
+
+            mylist.Add(73);
+            mylist.Add(17);
+            Console.WriteLine("count = {0}", mylist.Count);
+            Console.WriteLine("Capacity = {0}", mylist.Capacity);
+
+            mylist.Add(42);
+            Console.WriteLine("count = {0}", mylist.Count);
+            Console.WriteLine("Capacity = {0}", mylist.Capacity);
+
+            Console.WriteLine(mylist.GetHashCode());
+            double x = -7.5;
+            double y = 7.5;
+            Console.WriteLine(x.GetHashCode());
+            Console.WriteLine(y.GetHashCode());
+        }
+    }
+}
